ab504ac9d9a5144f2dcd8f71aee913093a85fba3
Paul Ingles <paul@oobaloo.co.uk>
2011-03-02 18:05:35 +0000

    Switch to using ring-core and add a sample app into the readme

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http "0.1.1"]
                  [clj-stacktrace "0.2.0"]
-                 [ring/ring "0.3.6"]]
+                 [ring/ring-core "0.3.6"]]
   :dev-dependencies [[enlive "1.0.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]
                      [midje "0.5.0"]])
