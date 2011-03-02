fed9823448287aa955d8a9273e216f2f3bff1dd9
Paul Ingles <paul@oobaloo.co.uk>
2011-03-02 15:13:54 +0000

    updated project.clj too with ring dependency; replace with ring-core in a bit

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http "0.1.1"]
-                 [clj-stacktrace "0.2.0"]]
+                 [clj-stacktrace "0.2.0"]
+                 [ring/ring "0.3.6"]]
   :dev-dependencies [[enlive "1.0.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]
                      [midje "0.5.0"]])
