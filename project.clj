438f9d2db704268bb7aaf000aae94d9f02684f44
Ben Mabey <ben@benmabey.com>
2011-09-02 14:44:23 -0600

    releases v0.1.4 with Airbrake change and id parsing fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-hoptoad "0.1.3"
+(defproject clj-hoptoad "0.1.4"
   :description "Hoptoad Client"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
