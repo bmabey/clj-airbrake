1e03443e59f128ffc26f66edb68bf73e33741e7d
John David Eriksen <johndavid@leadtune.com>
2012-08-10 13:41:33 -0400

    conforms to changes in id and error-id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-airbrake "0.1.4"
+(defproject clj-airbrake "0.1.5-LT"
   :description "Airbrake Client"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
