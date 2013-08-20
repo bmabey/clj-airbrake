ffc6c75efda4f2ba9c8b510c74028cac216c86e0
christianblunden <christian.blunden@gmail.com>
2013-08-20 16:25:22 +0100

    update to lein 2. add target to gitignore

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject clj-airbrake "2.1.0"
+(defproject clj-airbrake/clj-airbrake "2.1.0"
   :description "Airbrake Client"
+  :min-lein-version "2.0.0"
   :dependencies [[clj-http "0.7.6"]
                  [clj-stacktrace "0.2.6"]
                  [ring/ring-core "0.3.6"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.7"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]
-                     [enlive "1.0.0-SNAPSHOT"]
-                     [swank-clojure "1.3.4"]
-                     [midje "1.3.1"]])
+  :profiles {:dev
+             {:resource-paths ["test-resources"],
+              :dependencies
+              [[enlive "1.0.0-SNAPSHOT"] [midje "1.3.1"]]}}
+  :plugins [[lein-swank "1.4.1"] [lein-clojars "0.6.0"]])
