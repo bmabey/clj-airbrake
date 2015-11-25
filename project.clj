4a685d997eb7e776a013d55462553ea2f05e9a5d
Hugo Firth <hugo.firth@uswitch.com>
2015-11-25 14:27:18 +0000

    bumped project version and deps versions

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject clj-airbrake "2.4.3"
+(defproject clj-airbrake "3.0.0"
   :description "Airbrake Client"
   :min-lein-version "2.0.0"
   :url "https://github.com/bmabey/clj-airbrake"
-  :dependencies [[http-kit "2.1.11"]
-                 [clj-stacktrace "0.2.6"]
+  :dependencies [[http-kit "2.1.19"]
+                 [clj-stacktrace "0.2.8"]
                  [ring/ring-core "1.2.0"]
                  [org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]]
   :profiles {:dev
-             {:resource-paths ["test-resources"],
-              :dependencies
-              [[enlive "1.1.4"]
-               [midje "1.5.1"]]}})
+             {:resource-paths ["test-resources"]
+              :dependencies [[enlive "1.1.4"]
+                             [midje "1.5.1 "]]}})
