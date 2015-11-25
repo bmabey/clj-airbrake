7e7f3ea7ab5e7c8bbf68eb09c865f4507b193f8a
Hugo Firth <hugo.firth@uswitch.com>
2015-11-25 14:49:42 +0000

    updated ring handler

--- src/clj_airbrake/ring.clj
+++ src/clj_airbrake/ring.clj
@@ -5,29 +5,21 @@
 (defn request-to-message
   "Maps the ring request map to the format of the airbrake params"
   [{:keys [query-string] :as req}]
-  {:url (str (name (:scheme req))
-             "://"
-             (:server-name req)
-             (:uri req)
-             (when query-string (str "?" (codec/url-decode query-string))))
-   :component "component"
-   :action "action"
-   :cgi-data (get req :headers {})
+  {:context {:url (str (name (:scheme req))
+                       "://"
+                       (:server-name req)
+                       (:uri req)
+                       (when query-string (str "?" (codec/url-decode query-string))))
+             :headers (get req :headers {})}
    :params (or (:params req)
                {:query-string query-string})
    :session (get req :session {})})
 
 (defn wrap-airbrake
   "Catches exceptions and sends Airbrake notification."
-  ([handler api-key]
-     (wrap-airbrake handler api-key "development"))
-  ([handler api-key environment-name]
-     (wrap-airbrake handler api-key environment-name request-to-message))
-  ([handler api-key environment-name request-mapper]
+  ([handler airbrake-config]
+     (wrap-airbrake handler airbrake-config request-to-message))
+  ([handler airbrake-config request-mapper]
      (fn [req]
-       (with-airbrake {:api-key api-key
-                       :environment-name environment-name
-                       :project-root (System/getProperty "user.dir")}
-                      (request-mapper req)
-                      (handler req)
-                      ))))
+       (with-airbrake airbrake-config (request-mapper req)
+         (handler req)))))
